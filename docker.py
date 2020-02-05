097e191ebda4ab0305044c37573d109c81b17eb5
肖大人 <44285381+jiaowoxiaotete@users.noreply.github.com>
2020-02-05 17:45:08 +0800

    fix bug

--- docker.py
+++ docker.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding:utf-8 -*-
+#coding=utf-8
 import os
 import sys
 def massage():
@@ -59,7 +60,7 @@ if Num == 1:#运行容器新版端口偏移
         port_sev =  port_web
     else:
         port_sev = int(port_web) + int(port_ob)
-        print('服务器端口为:',port_sev)
+        print '节点端口为:',port_sev
         port_sev = str(port_sev) 
     url = raw_input('请输入网站地址(默认https://www.baidu.com):')
     if len(url) == 0:
@@ -94,7 +95,7 @@ if Num == 2:#运行容器新版端口偏移-解锁Netflix
         port_sev = port_web
     else:
         port_sev = int(port_web) + int(port_ob)
-        print('服务器端口为:',port_sev)
+        print '节点端口为:',port_sev
         port_sev = str(port_sev)
     dns = raw_input('请输入DNS服务器地址(默认172.81.99.87):')  
     if len(dns) == 0:
