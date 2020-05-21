dc103a0616fccad7a3bea55a5b6d0129fd875b1e
jiaowoxiaotete <jiaowoxiaotete@gamil.com>
2020-05-21 16:27:12 +0800

    Update docker.py

--- docker.py
+++ docker.py
@@ -57,6 +57,9 @@ if Num == 1:#运行容器新版端口偏移
     port_ob = raw_input('请输入端口偏移量(默认-10000):')
     if len(port_ob) == 0:
         port_sev =  '-10000'
+        port_sev = int(port_web) + int(port_sev)
+        print '节点端口为:',port_sev
+        port_sev = str(port_sev)
     else:
         port_sev = int(port_web) + int(port_ob)
         print '节点端口为:',port_sev
@@ -92,6 +95,9 @@ if Num == 2:#运行容器新版端口偏移-解锁Netflix
     port_ob = raw_input('请输入端口偏移量(默认-10000):')  
     if len(port_ob) == 0:
         port_sev = '-10000'
+        port_sev = int(port_web) + int(port_sev)
+        print '节点端口为:',port_sev
+        port_sev = str(port_sev)
     else:
         port_sev = int(port_web) + int(port_ob)
         print '节点端口为:',port_sev
